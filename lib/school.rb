aaf3ae9b8896fa8b145235e0f0add1afa41b950a
dwisecar <dwisecar@gmail.com>
2020-11-18 15:59:03 -0800

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+require 'pry'
+class School
+    attr_accessor :roster
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if self.roster.key?(grade)
+            self.roster[grade] << name
+        else
+            self.roster[grade] = []
+            self.roster[grade] << name
+        end
+
+    end
+
+    def grade(num)
+        if self.roster.key?(num)
+            self.roster[num]
+        end
+    end
+
+    def sort
+        sorted_roster = self.roster.transform_values! {|value| value.sort }
+    end
+
+
+end
+
